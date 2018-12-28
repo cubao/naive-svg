f11df1df16d8a86a71b8a706e6678e48ca6410b7
Tang Zhixiong <zhixiong@momenta.ai>
2018-12-28 13:48:41 +0800

    can install naive SVG

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='naive_svg',
+      version='0.0.1',
+      description='naive svg',
+      author='TANG ZhiXiong',
+      author_email='tang.zhi.xiong@qq.com',
+      url='https://github.com/cubao/naive-svg',
+      py_modules=['naive_svg'],
+      )
